9fd4f0fdc37f7e0a172af2d06e818875a8f09e35
Prateep Kul <1.0@kul.asia>
2020-02-12 01:55:34 -0600

    Fixed test

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -6,21 +6,22 @@ RSpec.describe Note, type: :model do
 
     it "valid with  both title and body" do
       before_count = Note.count
-      n1 = Note.new(title: "dddd", body: "body")
-      n1.save
+      n = Note.new(title: "dddd", body: "body")
+      n.save
       expect(Note.count).not_to eq(before_count)
     end
 
     it "valid with  only title" do
       before_count = Note.count
-      n2 = Note.new(title: "Title")
-      n2.save
+      n = Note.new(title: "Title")
+      n.save
       expect(Note.count).not_to eq(before_count)
     end
 
     it "invalid with  only body" do
       before_count = Note.count
-      Note.new(title: "Title")
+      n = Note.new(body: "body")
+      n.save
       expect(Note.count).to eq(before_count)
     end
   
@@ -28,7 +29,8 @@ RSpec.describe Note, type: :model do
       before_count = Note.count
       title_max = "x"*30
       body_max  = "y"*1001
-      Note.new(title: title_max, body: body_max)
+      n = Note.new(title: title_max, body: body_max)
+      n.save
       expect(Note.count).to eq(before_count)
     end
   end
